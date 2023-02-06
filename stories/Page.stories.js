712796ae49f728d753f4747c5b2f350addca39f5
Alexandre Grenier <aggrenier@gmail.com>
2023-02-05 22:23:13 -0500

    remove defaults

--- stories/Page.stories.js
+++ stories/Page.stories.js
@@ -1,19 +1,19 @@
-import { Page } from './Page';
-import * as HeaderStories from './Header.stories';
+// import { Page } from './Page';
+// import * as HeaderStories from './Header.stories';
 
-export default {
-  title: 'Example/Page',
-};
+// export default {
+//   title: 'Example/Page',
+// };
 
-const Template = (args) => Page(args);
+// const Template = (args) => Page(args);
 
-export const LoggedIn = Template.bind({});
-LoggedIn.args = {
-  // More on composing args: https://storybook.js.org/docs/web-components/writing-stories/args#args-composition
-  ...HeaderStories.LoggedIn.args,
-};
+// export const LoggedIn = Template.bind({});
+// LoggedIn.args = {
+//   // More on composing args: https://storybook.js.org/docs/web-components/writing-stories/args#args-composition
+//   ...HeaderStories.LoggedIn.args,
+// };
 
-export const LoggedOut = Template.bind({});
-LoggedOut.args = {
-  ...HeaderStories.LoggedOut.args,
-};
+// export const LoggedOut = Template.bind({});
+// LoggedOut.args = {
+//   ...HeaderStories.LoggedOut.args,
+// };
